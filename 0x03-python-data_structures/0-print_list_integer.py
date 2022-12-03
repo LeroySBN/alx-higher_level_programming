0d9aaa37f4a9988b2352503cadf206ef20bc9597
leroysb <leroybuliro@gmail.com>
2022-12-03 17:56:42 +0300

    0x03 task0 fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        print(str.format("{0}", my_list[i]))
+        print(str.format("{0:d}", my_list[i]))
