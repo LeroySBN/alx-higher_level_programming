fe712837eee32ce3315342beeef3bd07b2b20736
leroysb <leroybuliro@gmail.com>
2022-12-06 00:40:34 +0300

    task8.2 add

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,4 @@ def multiple_returns(sentence):
     if sentence == "":
         return (len(sentence), None)
     else:
-        length = len(sentence)
-        first = sentence[0]
-        return (length, first)
+        return (len(sentence), sentence[0])
