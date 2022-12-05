a899ac1e663aacb1e2140f8378ca44d72cc42893
leroysb <leroybuliro@gmail.com>
2022-12-06 02:46:51 +0300

    task9 add

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -8,3 +8,7 @@ print("Max: {}".format(max_value))
 my_list = []
 max_value = max_integer(my_list)
 print("Max: {}".format(max_value))
+
+my_list = [4]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
