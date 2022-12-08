237bee2a96e61018e39d6bfb95ecb4990e568a24
leroysb <leroybuliro@gmail.com>
2022-12-08 15:37:48 +0300

    task8 requirement fix

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
-    return a_dictionary
+    new_dict = a_dictionary.copy()
+    if key in new_dict:
+        del new_dict[key]
+    return new_dict
