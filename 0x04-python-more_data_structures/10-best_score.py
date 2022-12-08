110a11aac31c2dcf8c69888e933305a6f1cf1cd8
leroysb <leroybuliro@gmail.com>
2022-12-08 15:49:54 +0300

    task10 requirement fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    vscore = 0
-    kscore = None
-    for k, v in a_dictionary.items():
-        if vscore < v:
-            vscore = v
-            kscore = k
-    return kscore
+    if a_dictionary:
+        return (max(a_dictionary, key = a_dictionary.get))
