f6911e5bb76022230daac77f0df1fb115b2c4d38
leroysb <leroybuliro@gmail.com>
2022-12-19 17:52:31 +0300

    task0.2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,11 +4,11 @@ def safe_print_list(my_list=[], x=0):
         list1 = ""
         for elem in range(x):
             list1 += str(my_list[elem])
-        print(list1[0:x])
+        #print(list1[0:x])
         return (x)
     except IndexError:
         list1 = ""
         for i,elem in enumerate(my_list):
             list1 += str(elem)
-        print(list1)
+        #print(list1)
         return (i+1)
