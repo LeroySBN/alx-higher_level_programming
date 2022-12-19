7faba3c2e3d256dc787716eb10749e9f7d5c29aa
leroysb <leroybuliro@gmail.com>
2022-12-19 17:28:35 +0300

    task0.1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    indx = 0
     try:
         list1 = ""
         for elem in range(x):
             list1 += str(my_list[elem])
         print(list1[0:x])
-        indx = x
+        return (x)
     except IndexError:
         list1 = ""
         for i,elem in enumerate(my_list):
             list1 += str(elem)
         print(list1)
-        indx = (i+1)
-    return (indx)
+        return (i+1)
