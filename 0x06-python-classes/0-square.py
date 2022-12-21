2c7cd6fbe8fa49f17abfe4d41da648672c45a0fd
leroysb <leroybuliro@gmail.com>
2022-12-21 13:04:54 +0300

    task0.2

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Defines a class Square"""
 class Square:
-    """Defines a Square"""
-    def Square():
-        """Square function"""
+    """Defines a Square method"""
+    def square():
+        """Square method
+        Args:
+            None"""
         pass
     pass
