6e8363d9ffb9dea3092483ceea1512b6d39b20c3
leroysb <leroybuliro@gmail.com>
2022-12-21 14:44:10 +0300

    task1.2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,14 +2,14 @@
 """Defines a class Square"""
 
 
-class Square(size):
+class Square:
     """Defines a Square instance"""
 
     def __init__ (self, size):
         """The Square __init__ method
 
         Args:
-            size (int): size of the square
+            size (int): size of the square - private
 
         """
         self.__size = size
