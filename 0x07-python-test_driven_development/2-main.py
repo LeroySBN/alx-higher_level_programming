a5de2884d5cd3462cb2443b5edab791748875767
leroysb <leroybuliro@gmail.com>
2023-01-02 21:12:38 +0300

    task1

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -7,3 +7,61 @@ matrix = [
 ]
 print(matrix_divided(matrix, 3))
 print(matrix)
+
+print("------")
+print("------")
+
+try:
+    print(matrix_divided(matrix, "School"))
+except Exception as e:
+    print(e)
+
+print("------")
+print("------")
+
+try:
+    print(matrix_divided(matrix, 0))
+except Exception as e:
+    print(e)
+
+print("------")
+print("------")
+
+try:
+    print(matrix_divided(10, 2))
+except Exception as e:
+    print(e)
+
+print("------")
+print("------")
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8]
+]
+
+try:
+    print(matrix_divided(matrix, 1))
+except Exception as e:
+    print(e)
+
+print(matrix)
+
+
+print("------")
+print("------")
+
+
+matrix = [
+    [1, 2, 3],
+    ["This", "is", "Python"]
+]
+
+try:
+    print(matrix_divided(matrix, 1))
+except Exception as e:
+    print(e)
+
+print(matrix)
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module 2-matrix_divided
+Defines a matrix divider function
+
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Function that divides all elements of a matrix
+    """
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
+    elif type(matrix) is not list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    else:
+        for row in matrix:
+            if len(row) == len(matrix[0]):
+                pass
+            else:
+                raise TypeError("Each row of the matrix must have the same size")
+        for row in matrix:
+            for elem in row:
+                if type(elem) is not int and type(elem) is not float:
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        return list(map(lambda i: list(map(lambda x: round(x/div, 2), i)), matrix))
