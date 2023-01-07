7ef0a79660dcca6f32a540e72a683b38cd70234d
leroysb <leroybuliro@gmail.com>
2023-01-08 01:53:35 +0300

    task2&3

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-Module 4-print_square
-prints a square with character #
-"""
+"""Module 4-print_square"""
 
 
 def print_square(size):
     """
-    prints a square
+    prints a square with character #
     """
-    if not isinstance(size, (int, float)) and size < 0:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
