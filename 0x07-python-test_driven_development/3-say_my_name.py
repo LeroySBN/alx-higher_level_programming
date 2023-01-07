97ff86723204821866568c4084f5d5a8bfd65be6
leroysb <leroybuliro@gmail.com>
2023-01-08 01:17:59 +0300

    task2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,8 +10,8 @@ def say_my_name(first_name, last_name=""):
     """
     Prints name
     """
-    if type(first_name) is not str or first_name == "" or first_name == " ":
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    elif type(last_name) is not str or last_name == " ":
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     return print(f"My name is {first_name} {last_name}")
