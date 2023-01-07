6c74ec6e82725462d4cff9d05df4ceb1b2b28fdd
leroysb <leroybuliro@gmail.com>
2023-01-07 19:59:06 +0300

    ign

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -5,6 +5,7 @@ print(add_integer(1, 2))
 print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
+print(add_integer(1.0, 2.6))
 try:
     print(add_integer(4, "School"))
 except Exception as e:
@@ -13,7 +14,9 @@ try:
     print(add_integer(None))
 except Exception as e:
     print(e)
-try:
-    print(add_integer())
-except Exception as e:
-    print(e)
+
+print(add_integer(1.0, 2.3))
+print(add_integer(1.0, -2.0))
+print(add_integer(-1, 2))
+print(add_integer(-1.0, 2.0))
+print(add_integer(-1.0, -2.0))
