b820f8aca702a9e3d94b5d733f98aa2868cec56a
leroysb <leroybuliro@gmail.com>
2023-01-07 17:52:02 +0300

    task0.2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,20 +9,10 @@ Defines a sum function
 def add_integer(a, b=98):
     """
     Sum function add_integer that returns sum of a and b
-
-    Args:
-        a (int): first value
-        b (int): second value
-
-    Returns:
-        sum of a and b
     """
-    if type(a) is not int and type(a) is not float:
+    if  not isinstance (a, (int, float)):
         raise TypeError("a must be an integer")
-    elif type(b) is not int and type(b) is not float:
+    elif not isinstance (b, (int, float)):
         raise TypeError("b must be an integer")
-    elif not a:
-        raise TypeError("a must be an integer")
     else:
-        sum = int(a) + int(b)
-    return sum
+        return int(a) + int(b)
