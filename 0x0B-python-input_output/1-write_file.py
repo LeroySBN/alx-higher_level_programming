23019886e370b3dcba74f7713dfd9330a5b1f19e
leroysb <leroybuliro@gmail.com>
2023-01-11 01:55:47 +0300

    task1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 1-write_file"""
+
+
+def write_file(filename="", text=""):
+    """Function that writes a string to a text file
+
+    Returns:
+        number of characters written
+    """
+    with open(filename, mode='w', encoding="utf-8") as f:
+        f.write(text)
+
+    return len(text)
