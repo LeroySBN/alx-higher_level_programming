6c23b238ad9c77a4d498be4ef1116a6ed5f945eb
leroysb <leroybuliro@gmail.com>
2023-01-11 18:00:09 +0300

    task7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 7-add_item"""
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+f = 'add_item.json'
+
+if __name__ == "__main__":
+    try:
+        save_to_json_file(load_from_json_file(f) + argv[1:], f)
+    except (FileNotFoundError, ValueError):
+        save_to_json_file(argv[1:], f)
