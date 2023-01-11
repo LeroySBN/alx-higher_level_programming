49852dd38586aa9aba12f7cde6e5484c9a1dc97a
leroysb <leroybuliro@gmail.com>
2023-01-11 18:10:36 +0300

    task8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module 8-class_to_json"""
+
+
+def class_to_json(obj):
+    """Function that returns the dictionary description with simple data structure for JSON serialization of an object"""
+    return obj.__dict__
