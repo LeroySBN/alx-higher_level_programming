6a06248402d2d1f75527311c100cb25cb6fc7b7d
leroysb <leroybuliro@gmail.com>
2023-03-13 21:42:05 +0300

    task0-4

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myStr1 = 'C is fun';
+console.log(myStr1);
+const myStr2 = 'Python is cool';
+console.log(myStr2);
+const myStr3 = 'JavaSCript is amazing';
+console.log(myStr3);
