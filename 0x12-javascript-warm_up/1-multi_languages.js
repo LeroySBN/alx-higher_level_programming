cafa106e9258d5c230ad165c41e2145473de2b1f
leroysb <leroybuliro@gmail.com>
2023-03-13 21:58:19 +0300

    task1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-const myStr1 = 'C is fun';
-console.log(myStr1);
-const myStr2 = 'Python is cool';
-console.log(myStr2);
-const myStr3 = 'JavaSCript is amazing';
-console.log(myStr3);
+const myStr = ['C is fun', 'Python is cool', 'JavaSCript is amazing']
+console.log(myStr[0] + '\n' + myStr[1] + '\n' + myStr[2]);
