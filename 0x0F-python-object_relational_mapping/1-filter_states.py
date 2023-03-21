bf57cb5e34d50965550365301de4c76e619bd8d2
leroysb <leroybuliro@gmail.com>
2023-03-21 14:49:02 +0300

    req mix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,12 @@ if __name__ == '__main__':
     )
     cur = conn.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        "SELECT * \
+        FROM states \
+        WHERE CONVERT(`name` USING Latin1) \
+        COLLATE Latin1_General_CS \
+        LIKE 'N%' \
+        ORDER BY id ASC"
     )
     query_rows = cur.fetchall()
     for row in query_rows:
