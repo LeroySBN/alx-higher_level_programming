8fd12dea6877613a8fbfea89899103b2a931be40
leroysb <leroybuliro@gmail.com>
2023-04-10 13:58:30 +0300

    re-done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,9 +4,7 @@
 
 def find_peak(list_of_integers):
     """Find highest value in list of unsorted integers"""
-
     my_list = list_of_integers
-
     if my_list:
         my_list.sort()
         return my_list[-1]
