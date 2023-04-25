1b024d8bc45f3c8956f6b348267e6ae4a3b37266
leroysb <leroybuliro@gmail.com>
2023-04-25 15:04:17 +0300

    style fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,8 +5,8 @@ const file = process.argv[2];
 
 fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
-	console.log(err);
+    console.log(err);
   } else {
-	console.log(data);
+    console.log(data);
   }
 });
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,5 +3,5 @@
 const fs = require('fs');
 const request = require('request');
 
-request(process.argv[2]).
-	pipe(fs.createWriteStream(process.argv[3]));
+request(process.argv[2])
+  .pipe(fs.createWriteStream(process.argv[3]));
